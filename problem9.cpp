28755089b4fc40aea67ba23abb26f526cd27c033
Dilshodbek078 <d.orazbayev@newuu.uz>
2025-11-21 23:43:33 +0500

    Update problem9.cpp

--- problem9.cpp
+++ problem9.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+void factorial(int n, int *fact) {
+    *fact = 1;
+    for (int i = 1; i <= n; i++) {
+        *fact *= i;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int fact;
+    factorial(n, &fact);
+    cout << fact << endl;
+    return 0;
+}
